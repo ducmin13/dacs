804d6a9286c4826e704530bc641edf139d65af31
ducmin13 <ngoducminh1305@gmail.com>
2022-06-13 13:39:41 +0700

    Up lan 2

--- header.php
+++ header.php
@@ -4,63 +4,61 @@ include "/xampp/htdocs/doancoso/admin/class/header_class.php";
 
 $con = mysqli_connect("localhost", "root", "", "dacs");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TMDSport - Hệ thống Shop đồ thể thao uy tín</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<!--load jquery truoc khi load bootstrap js-->
+	<script src="jquery-3.3.1.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+</head>
+<body>
+    <!--start menu-->
+	<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+		<div class="container">
+			<a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
 
-<header>
-            <div class="logo">
-                <a class="fa-solid fa-medal" href="index.php"></a>
-            </div>
-                <div class="menu">
-                                <li><a href="giohang.php">Sản phẩm</a></li>
-                                <li><a href="giohang2.php">Sale</a></li>
-                                
-                    <ul class="sub-menu">
-                        <?php 
-                        $query = "SELECT brand_name FROM tbl_brand ORDER BY brand_id DESC ";     
-                        $result = mysqli_query($con, $query);                   
-                        if(mysqli_num_rows($result) > 0){
-                            while ($row = mysqli_fetch_array($result)){
-                        ?>
-                        <li> <a><?php echo $row['brand_name'] ?></a> </li>
-                        <?php                 
-                        }
-                       }                   
-                    ?>   
-                            <!-- <ul>
-                                <li><a href="">Giày Yonex</a></li>
-                            </ul>
-                        </li>                
-                        <li><a href="">Vợt cầu lông</a>               
-                            <ul>
-                                <li><a href="">Vợt Yonex</a></li>
-                                <li><a href="">Vợt Mizuno</a></li>
-                                <li><a href="">Vợt Victor</a></li>
-                                <li><a href="">Vợt Lining</a></li>
-                                <li><a href="">Vợt Kawasaki</a></li>
-                           </ul>                           
-                        </li>                        
-                    </ul>                                     
-                <li>
-                <li><a href="">THÔNG TIN</a></li>
-                <li><a href="">HƯỚNG DẪN</a></li>
-                <li><a href="">SALE</a></li>
-                <li><a href="">
-                    
-                </a></li> -->
-                
-                     
-            </div>
-            <div class="other">
-                <li><input placeholder="Tìm Kiếm" type="text"> <i class="fas fa-search"></i></li>             
-                <li><a class="fa fa-shopping-bag" href="giohang2.php"></a></li>
-                <li ><a class="fa fa-user" href="./admin/formdangnhap.php"></a></li>
-                <?php 
-                if(isset($_SESSION))
-                       echo $_SESSION['username'];
-                else
-                       echo "Chưa đăng nhập";
-                ?>
-                <li><a class='fa-solid fa-arrow-right-from-bracket' href= "index.php?id=1" onclick=" return confirm('Bạn muốn đăng xuất?')" > </a> </li>   
-                              
-</div>
-        </header>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    	<li class="nav-item active">
+        <a class="nav-link" href="#">Trang chủ<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Giới thiệu<span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link" href="./giohang.php" id="navbarDropdown">
+          Sản phẩm
+        </a>
+        <div class="dropdown-content">
+          <a class="dropdown-item" href="#">Vợt cầu lông Yonex</a>
+          <a class="dropdown-item" href="#">Vợt cầu lông Victor</a>
+        </div>
+      </li>
      
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Chính sách<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="./admin/formdangnhap.php">Đăng nhập<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="./admin/formdangky.php">Đăng ký<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    </div>
+		</div>
+    </nav>
+    <!--end menu-->
+
+    
+ 
--- index.php
+++ index.php
@@ -1,110 +1,43 @@
 <?php session_start(); ?>
 <?php 
     include "header.php";
+    include "footer.php";
     
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/f8d037ce4a.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="main.php" media="screen">
-    <title>TMDSport</title>
-</head>
-<body  >
- <!-- ---------slider-------- -->
-        <section id="Slider">
-<div class="aspect-ratio-169">
-        <img src="images/25(2).jpg">
-        <img src="images/33333333.jpg">
-        <img src="images/444444444444444.jpg">
-        <img src="images/6.jpg">
-        <img src="images/yonex-astrox88-1920x812px-1400x592.jpg">
-</div>
-<div class="dot-container">
-    <div class="dot active"></div>
-    <div class="dot"></div>
-    <div class="dot"></div>
-    <div class="dot"></div>
-    <div class="dot"></div>
-</div>
-</section>
-<!-- ****************apppppppppppppp telephone***************** -->
-<section class="app-BadmintonSport">
-    <p>Tải ứng dụng TMDSport</p>
-    <div class="Download-App">
-        <img src="https://pubcdn2.ivymoda.com/images/googleplay.png">
-        <img src="https://pubcdn2.ivymoda.com/images/appstore.png" >
+     <!--start slide anh-->
+     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+         <ol class="carousel-indicators">
+         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+         </ol>
+         <div class="carousel-inner">
+         <div style="width: 1920;height: 530;" class="carousel-item active">
+        <img class="d-block w-100" src="img/hinh1.jpg" alt="First slide">
+        </div>
+        <div style="width: 1920;height: 530;" class="carousel-item">
+        <img class="d-block w-100" src="img/hinh2.jpg" alt="Second slide">
+         </div>
+        <div style="width: 1920;height: 530;" class="carousel-item">
+          <img class="d-block w-100" src="img/hinh3.jpg" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-    <p>Nhận thông báo TMDSport</p>
-    <input type="text" placeholder="Nhập email của bạn...">
+     <!--end slide anh-->
 
-</section>
-<!-- ****************footerrrrrrrr cuối***************** -->
-<div class="footer-top">
-    <li><a href=""><img src="https://shopvnb.com/templates/images/bct.png" width="120" height="70" alt=""></a></li>
-    <li><a href=""></a>Liên hệ</li>
-    <li><a href=""></a>Tuyển dụng</li>
-    <li><a href=""></a>Giới thiệu</li>
-    <li>
-        <a href="" class="fab fa-facebook-f"></a>
-        <a href="" class="fab fa-twitter"></a>
-        <a href="" class="fab fa-youtube"></a>
-    </li>
-</div>
-<div class="footer-center">
-    <p>Công ty thể thao Uy tín - Chất lượng - Số 1 Việt Nam TMD Sport <br>
-        Địa chỉ đăng ký: 19DTHA3, Đồ án cơ sở cuối kì , Hutech khu E, Xa lộ hà nội, Việt Nam - 0839101201 <br>
-        Đặt hàng online tại: <b>0888323441</b> 
-        </p> 
-</div>
 
-<div class="footer-bottom">
-    @TMDSport The Best Shop
-</div>
+  
+		
+
+	
 </body>
-
-<script>
-// chỉnh vị trí jvscrip
-const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
-const imgContainer = document.querySelector('.aspect-ratio-169')
-const dotItem = document.querySelectorAll(".dot")
-let imgNumber = imgPosition.length
-let index = 0
-// console.log(imgPosition) để xem nó đã nhận được chưa
-// Dùng foreach để căn chỉnh 
-imgPosition.forEach(function(image,index){
-    // căn theo chiều ngang câu lệnh dưới 
-    image.style.left = index*100 + "%"
-    dotItem[index].addEventListener("click",function(){
-    slider(index)
-
-    })
-    // cho nó auto andgogo
-
-})
-// tạo phần tử let cho nó trượt và cho index =0 dùng function cộng nó lên
-function imgSlide (){
-    index ++;
-    console.log(index)
-    if (index >=imgNumber){index =0}
-    slider(index)
-    
-    
-    
-}
-function slider (index) {
-    // nhân lên cho thẻ con chạy với 3s 1 lượt chuyển
-    imgContainer.style.left = "-" +index*100+ "%"
-    // slide tới đâu thì chạy dot tới đó
-    const dotActive = document.querySelector('.active')
-    dotActive.classList.remove("active")
-    dotItem[index].classList.add("active")
- 
-}
-//   setInterval(imgSlide,4000)
-</script>
 </html>
